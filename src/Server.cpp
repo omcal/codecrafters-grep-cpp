c39eaaaedabf36209bba25af0bbd8ecdd4164e33
omcal <omerisildak0@gmail.com>
2024-10-27 19:40:45 +0300

    codecrafters submit [skip ci]

--- src/Server.cpp
+++ src/Server.cpp
@@ -10,6 +10,7 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
     }
 }
 
+
 int main(int argc, char* argv[]) {
     // Flush after every std::cout / std::cerr
     std::cout << std::unitbuf;
@@ -31,19 +32,20 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+
     // Uncomment this block to pass the first stage
     //
-    // std::string input_line;
-    // std::getline(std::cin, input_line);
+    std::string input_line;
+    std::getline(std::cin, input_line);
     //
-    // try {
-    //     if (match_pattern(input_line, pattern)) {
-    //         return 0;
-    //     } else {
-    //         return 1;
-    //     }
-    // } catch (const std::runtime_error& e) {
-    //     std::cerr << e.what() << std::endl;
-    //     return 1;
-    // }
+    try {
+    	if (match_pattern(input_line, pattern)) {
+             return 0;
+         } else {
+             return 1;
+         }
+     } catch (const std::runtime_error& e) {
+         std::cerr << e.what() << std::endl;
+         return 1;
+     }
 }
